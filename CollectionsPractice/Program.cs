5fcb3d4ca39830688b86aa0192557579b8ea06e4
Bryce Eaton <bryce.eaton86@gmail.com>
2021-08-13 15:25:09 -0500

    initial commit

---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercises:
+            //  //   Three Basic Arrays
+            //     Create an array to hold integer values 0 through 9
+            // int[] numArray = new int[10];
+            // for (int index = 0; index  < numArray.Length; index++)
+            // {
+            //     numArray[index] = index;
+            // }
+            // foreach (int index in numArray)
+            // {
+            //     Console.WriteLine(index);
+            // }
+
+            //  //   Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+            string[] nameArray = new string[4];
+            nameArray[0] = "Tim";
+            nameArray[1] = "Martin";
+            nameArray[2] = "Nikki";
+            nameArray[3] = "Sara";
+            // foreach (string name in nameArray)
+            // {
+            //     System.Console.WriteLine(name);
+            // }
+
+            //  //   Create an array of length 10 that alternates between true and false values, starting with true
+            // bool[] tAndF = new bool[10];
+            // for(int index = 0; index < tAndF.Length; index++)
+            // {
+            //     if (index%2 == 0) 
+            //     {
+            //         tAndF[index] = true;
+            //     }
+            //     else
+            //     {
+            //         tAndF[index] = false;
+            //     }
+            // }
+            // foreach (bool result in tAndF)
+            // {
+            //     System.Console.WriteLine(result);
+            // }
+
+            //  //   List of Flavors
+            //  //   Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
+                List<string> flavors = new List<string>();
+                flavors.Add("Mint Chocolate Chip");
+                flavors.Add("Chocolate");
+                flavors.Add("Vanilla");
+                flavors.Add("Cookies 'n Cream");
+                flavors.Add("Butter Pecan");
+            // //     Output the length of this list after building it
+            // System.Console.WriteLine(flavors.Count);
+            // //     Output the third flavor in the list, then remove this value
+            // string vanilla = flavors[2];
+            // System.Console.WriteLine(vanilla);
+            // flavors.Remove(vanilla);
+            // //     Output the new length of the list (It should just be one fewer!)
+            // System.Console.WriteLine(flavors.Count);
+
+
+            //     User Info Dictionary
+            //     Create a dictionary that will store both string keys as well as string values
+            //     Add key/value pairs to this dictionary where:
+            //     each key is a name from your names array
+            //     each value is a randomly select a flavor from your flavors list.
+
+            Random rand = new Random();
+            Dictionary<string,string> profile = new Dictionary<string,string>();
+            foreach (string name in nameArray)
+            {
+                // Console.WriteLine($"My name is {name} and my favorite ice cream is {flavors[rand.Next(0, flavors.Count)]} ");
+                profile.Add(name, flavors[rand.Next(0, flavors.Count)]);
+
+            }
+            //     Loop through the dictionary and print out each user's name and their associated ice cream flavor
+            foreach (var entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+    }
+}
---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Variable to interpolate
+            string place = "Coding Dojo";
+            //Interpolated string, note the $
+            string message = $"Hello {place}";
+            // Displaying final message
+            Console.WriteLine(message);
+
+            // Console.WriteLine("The value of \r\n pi is " + 3.14159);
+
+            // Console.WriteLine("My name is {0}, I am " + 28 + " years old", "Tim");
+            // Console.WriteLine($"My name is {0}, I am " + 28 + " years old", "Tim");
+
+
+            int numRings = 5;
+            string name = "Kobe";
+            if (numRings >= 5 && name == "Kobe")
+            {
+                Console.WriteLine("You are welcome to join the party");
+            }
+            else if (numRings > 2)
+            {
+                Console.WriteLine($"Decent...but {numRings} rings aren't enough");
+            }
+            else
+            {
+                Console.WriteLine("Go win some more rings");
+            }
+
+
+            // int start = 1;
+            // int end = 100;
+            // // loop from start to end including end
+            // for (int index = start; index <= end; index++)
+            // {
+            //     if (index == 3 || index == 5 )
+            //     {
+            //         if (index == 3)
+            //         {
+            //             Console.WriteLine("Fizz");
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine("FizzBuzz");
+            //         }
+            //     }
+            //     else 
+            //     {
+            //         Console.WriteLine(index);
+            //     }
+            // }
+
+            // int i = 1;
+            // while (i < 256)
+            // {
+            //     Console.WriteLine(i);
+            //     i = i + 1;
+            // }
+
+            // Random rand = new Random();
+            // for(int val = 0; val < 10; val++)
+            // {
+            //     //Prints the next random value between 2 and 8
+            //     Console.WriteLine(rand.Next(2,8));
+            // }
+
+            // string[] myCars = new string[] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"}; 
+            // foreach (string car in myCars)
+            // {
+            //     // We no longer need the index, because variable 'car' already represents each indexed value
+            //     Console.WriteLine($"I own a {car}");
+            // }
+
+
+            //Initializing an empty list of Motorcycle Manufacturers
+            List<string> bikes = new List<string>();
+            //Use the Add function in a similar fashion to push
+            bikes.Add("Kawasaki");
+            bikes.Add("Triumph");
+            bikes.Add("BMW");
+            bikes.Add("Moto Guzzi");
+            bikes.Add("Harley Davidson");
+            bikes.Add("Suzuki");
+            //Accessing a generic list value is the same as you would an array
+            Console.WriteLine(bikes[2]); //Prints "BMW"
+            Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
+            Console.WriteLine("The current manufacturers we have seen are:");
+            for (var idx = 0; idx < bikes.Count; idx++)
+            {
+                Console.WriteLine("-" + bikes[idx]);
+            }
+            // Insert a new item between a specific index
+            bikes.Insert(2, "Yamaha");
+            //Removal from Generic List
+            //In this case we are removing all manufacturers located in Japan
+            bikes.Remove("Suzuki");
+            bikes.Remove("Yamaha");
+            bikes.RemoveAt(0); //RemoveAt has no return value however
+            //The updated list can then be iterated through using a foreach loop
+            Console.WriteLine("List of Non-Japanese Manufacturers:");
+            foreach (string manu in bikes)
+            {
+                Console.WriteLine("-" + manu);
+            }
+
+            Dictionary<string,string> profile = new Dictionary<string,string>();
+            //Almost all the methods that exists with Lists are the same with Dictionaries
+            profile.Add("Name", "Speros");
+            profile.Add("Language", "PHP");
+            profile.Add("Location", "Greece");
+            Console.WriteLine("Instructor Profile");
+            Console.WriteLine("Name - " + profile["Name"]);
+            Console.WriteLine("From - " + profile["Location"]);
+            Console.WriteLine("Favorite Language - " + profile["Language"]);
+            foreach (var entry in profile)  // var takes the place of KeyValuePair<string,string>
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+
+
+        }
+    }
+}
